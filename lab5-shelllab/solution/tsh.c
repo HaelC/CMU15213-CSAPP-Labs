a9bbd1cc96a11d8b615bc1706ed67e225ab87e0a
HaelChen <f.procumbens@gmail.com>
2020-10-03 20:03:12 -0700

    simple implementation from book

--- lab5-shelllab/solution/tsh.c
+++ lab5-shelllab/solution/tsh.c
@@ -165,6 +165,39 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS]; /* Argument list execve() */
+    char buf[MAXLINE];   /* Holds modified command line */
+    int bg;              /* Should the job run in bg or fg? */
+    pid_t pid;           /* Process id */
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+        return;   /* Ignore empty lines */
+
+    if (!builtin_cmd(argv)) {
+        pid = fork();
+        if (pid < 0) {
+            unix_error("Fork error");
+        }
+        if (pid == 0) {   /* Child runs user job */
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+
+        /* Parent waits for foreground job to terminate */
+        if (!bg) {
+            int status;
+            if (waitpid(pid, &status, 0) < 0) {
+                unix_error("waitfg: waitpid error");
+            }
+        }
+        else {
+            printf("%d %s", pid, cmdline);
+        }
+    }
     return;
 }
 
@@ -231,6 +264,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if (!strcmp(argv[0], "quit")) /* quit command */
+        exit(0);
+    if (!strcmp(argv[0], "&"))    /* Ignore singleton */
+        return 1;
     return 0;     /* not a builtin command */
 }
 
